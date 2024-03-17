741123e6eb96f94a3a4a90a46202c7f1a71b3c5c
antontitov <titov@qtim.ru>
2024-03-17 17:10:19 +0600

    refactor: optimize rto

--- src/modules/files/rto/file.rto.ts
+++ src/modules/files/rto/file.rto.ts
@@ -1,22 +1,14 @@
 import { IFile } from '../interfaces/file.interface';
 import { ApiProperty } from '@nestjs/swagger';
 import { CommonResponseRTO } from '../../../common/rto/common-response.rto';
+import { BaseEntity } from '../../../common/entities/base.entity';
 
-export class FileRTO implements IFile {
-  @ApiProperty()
-  createdAt: Date;
-
-  @ApiProperty()
-  id: string;
-
+export class FileRTO extends BaseEntity implements IFile {
   @ApiProperty()
   mimetype: string;
 
   @ApiProperty()
   size: number;
-
-  @ApiProperty()
-  updatedAt: Date;
 }
 
 export class OneFileRTO extends CommonResponseRTO<FileRTO> {
